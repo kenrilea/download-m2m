ec98f7bf2618c2e106f4bd09ddb76a7f9f5c4652
TheOneNite <travisrilea@gmail.com>
2021-06-18 22:35:20 -0400

    chore: added launcher zip for download

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import banner from './mars-banner-transition.png';
-import MissionToMars from './MissionToMars.csv';
-import './App.css';
+import React from 'react'
+import banner from './mars-banner-transition.png'
+import MissionToMars from './MissionToMars.zip'
+import './App.css'
 
 function App() {
   return (
     <div>
       <h1 className='banner-header unselectable'>Mission to Mars</h1>
       <img src={banner} className='banner-image unselectable' />
-      <a
-        href={MissionToMars}
-        className='download-link'
-        download
-      >
+      <a href={MissionToMars} className='download-link' download>
         Download Game
       </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
